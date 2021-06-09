1d0ecfd1e9b54a1355643609f15007e282fc275f
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-09 18:05:33 +0530

    Update ocr_gui.py

--- ocr_gui.py
+++ ocr_gui.py
@@ -4,9 +4,13 @@ from PIL import Image,ImageTk
 import random
 from winsound import  *
 """
-CREATED BY BISWARUP BHATTACHARJEE
-EMAIL    : bbiswa471@gmail.com
-PHONE NO : 6290272740
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
 """
 poklist=["pok.wav","pok2.wav"]
 clicked=lambda:PlaySound(random.choice(poklist),SND_FILENAME)
