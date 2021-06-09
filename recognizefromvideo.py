368f63408d688632b9060a0f410439e3704fb228
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-09 18:06:13 +0530

    Update recognizefromvideo.py

--- recognizefromvideo.py
+++ recognizefromvideo.py
@@ -5,9 +5,13 @@ import cv2
 from tkinter import filedialog
 #Browsing Only Video Files(.mp4 format)
 '''
-CREATED BY BISWARUP BHATTACHARJEE
-EMAIL    : bbiswa471@gmail.com
-PHONE NO : 6290272740
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
 '''
 def browsevideo():
     filename =filedialog.askopenfilename(filetypes=(("mp4 files","*.mp4"),("All files","*.*")))
